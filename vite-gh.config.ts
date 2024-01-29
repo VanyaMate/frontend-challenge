db9bee0784cc858bc380ad748e20adfcf61682c7
VanyaMate <buhu653@mail.ru>
2024-01-30 00:36:18 +0300

    git status

---
+++ vite-gh.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [ react() ],
+    base   : '/frontend-challenge',
+    css    : {
+        modules: {
+            generateScopedName: '[name]_[local]_[hash:base64:5]',
+        },
+    },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+});
